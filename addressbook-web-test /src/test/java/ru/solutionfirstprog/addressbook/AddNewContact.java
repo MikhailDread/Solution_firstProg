896cbf92edf75f73609f50ec13957f6819cecf89
ZmixZ <nuquq@yandex.ru>
2020-02-16 10:06:37 +0300

    Задание 4

--- addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/AddNewContact.java
+++ addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/AddNewContact.java
@@ -1,14 +1,11 @@
 package ru.solutionfirstprog.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class AddNewContact {
   private WebDriver driver;
@@ -16,48 +13,72 @@ public class AddNewContact {
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     driver = new ChromeDriver();
     baseUrl = "https://www.google.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.get("http://localhost/addressbook/delete.php?part=1;2;");
+    login("admin", "secret");
+  }
+
+  private void login(String log, String password) {
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys(log);
+    driver.findElement(By.id("LoginForm")).click();
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.id("LoginForm")).submit();
   }
 
   @Test
   public void testAddNewContact() throws Exception {
-    driver.get("http://localhost/addressbook/delete.php?part=1;2;");
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.id("LoginForm")).click();
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.id("LoginForm")).submit();
-    driver.findElement(By.linkText("add new")).click();
-    driver.findElement(By.name("firstname")).click();
-    driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys("Ivan");
-    driver.findElement(By.name("middlename")).click();
-    driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys("Ivanovich");
-    driver.findElement(By.name("lastname")).click();
-    driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys("Ivanov");
-    driver.findElement(By.name("company")).click();
-    driver.findElement(By.name("company")).clear();
-    driver.findElement(By.name("company")).sendKeys("RZD");
-    driver.findElement(By.name("address")).click();
-    driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys("Moscow, street Tambovskaya.");
-    driver.findElement(By.name("email")).click();
-    driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys("jonjolli@yandex.fu");
-    driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    gotoNewContact();
+    createUserInf(new UserInf("Ivan", "Ivanovich", "Ivanov"));
+    createContactInf(new ContactIng("RZD", "Moscow, street Tambovskaya.", "jonjolli@yandex.fu"));
+    submitCreation();
+    gotoHomePage();
+  }
+
+  private void gotoHomePage() {
     driver.findElement(By.linkText("home page")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  private void submitCreation() {
+    driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void createContactInf(ContactIng contactIng) {
+    driver.findElement(By.name("company")).click();
+    driver.findElement(By.name("company")).clear();
+    driver.findElement(By.name("company")).sendKeys(contactIng.getCompany());
+    driver.findElement(By.name("address")).click();
+    driver.findElement(By.name("address")).clear();
+    driver.findElement(By.name("address")).sendKeys(contactIng.getStreet());
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys(contactIng.getEmail());
+  }
+
+  private void createUserInf(UserInf userInf) {
+    driver.findElement(By.name("firstname")).click();
+    driver.findElement(By.name("firstname")).clear();
+    driver.findElement(By.name("firstname")).sendKeys(userInf.getName());
+    driver.findElement(By.name("middlename")).click();
+    driver.findElement(By.name("middlename")).clear();
+    driver.findElement(By.name("middlename")).sendKeys(userInf.getMiddlename());
+    driver.findElement(By.name("lastname")).click();
+    driver.findElement(By.name("lastname")).clear();
+    driver.findElement(By.name("lastname")).sendKeys(userInf.getLastname());
+  }
+
+  private void gotoNewContact() {
+    driver.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     driver.quit();
     String verificationErrorString = verificationErrors.toString();
---
+++ addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/ContactIng.java
@@ -0,0 +1,25 @@
+package ru.solutionfirstprog.addressbook;
+
+public class ContactIng {
+    private final String company;
+    private final String street;
+    private final String email;
+
+    public ContactIng(String company, String street, String email) {
+        this.company = company;
+        this.street = street;
+        this.email = email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}

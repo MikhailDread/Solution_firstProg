52c3d40c34bb380815b00cf183b673dd0590450e
ZmixZ <termixator@yandex.ru>
2020-02-21 22:21:12 +0300

    Задание 7

--- addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,11 @@
 package ru.solutionfirstprog.addressbook.appmanager;
 
+import javafx.application.Application;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,9 +17,21 @@ public class ApplicationManager{
     private NavigationClass navigationClass;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplicationManager(String browser){
+        this.browser = browser;
+    }
 
 
     public void init() {
+        if(browser.equals(BrowserType.CHROME)){
+            driver = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)){
+            driver = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.IE)){
+            driver = new InternetExplorerDriver();
+        }
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(driver);
--- addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/tests/TestBase.java
+++ addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.solutionfirstprog.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.solutionfirstprog.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager applicationManager = new ApplicationManager();
+    protected final ApplicationManager applicationManager = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {

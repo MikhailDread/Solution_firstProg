23041367334fb4bd224cbd48732a2d052174b956
ZmixZ <termixator@yandex.ru>
2020-02-24 19:21:22 +0300

    Задание 8

--- addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/tests/ContactModification.java
+++ addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/tests/ContactModification.java
@@ -2,7 +2,7 @@ package ru.solutionfirstprog.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.solutionfirstprog.addressbook.module.ContactIng;
-import ru.solutionfirstprog.addressbook.module.GroupInf;
+
 
 public class ContactModification extends TestBase {
 
@@ -10,10 +10,10 @@ public class ContactModification extends TestBase {
     public void testContactModification(){
 
         applicationManager.getReturnHelper().returnHome();
-        if(!applicationManager.getContactHelper().thereAContact()){  //если нет возможности нажать селектед
-            applicationManager.getNavigationClass().gotoNewContact(); //создание контакта
-            applicationManager.getContactHelper().createContact();//создание контакта 
-            applicationManager.getReturnHelper().gotoHomePage();//создание контакта
+        if(!applicationManager.getContactHelper().thereAContact()){
+            applicationManager.getNavigationClass().gotoNewContact();
+            applicationManager.getContactHelper().createContact();
+            applicationManager.getReturnHelper().gotoHomePage();
         }
         applicationManager.getContactHelper().tookContactModification();
         applicationManager.getContactHelper().createContactInf(new ContactIng("RZD", "Moscow, street Tambovskaya.", "jonjolli@yandex.fu", "Ivan", "Ivanovich", "Ivanov", null), false);
--- addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/tests/DeleteContact.java
+++ addressbook-web-test /src/test/java/ru/solutionfirstprog/addressbook/tests/DeleteContact.java
@@ -2,7 +2,7 @@ package ru.solutionfirstprog.addressbook.tests;
 
 
 import org.testng.annotations.*;
-import ru.solutionfirstprog.addressbook.module.ContactIng;
+
 
 
 public class DeleteContact extends TestBase{
@@ -11,10 +11,10 @@ public class DeleteContact extends TestBase{
   @Test
   public void testDeleteContact() throws Exception {
     applicationManager.getReturnHelper().returnHome();
-    if(!applicationManager.getContactHelper().thereAContact()){ //если нет возможности нажать селектед
-      applicationManager.getNavigationClass().gotoNewContact();  //создание контакта
-      applicationManager.getContactHelper().createContact();   //создание контакта
-      applicationManager.getReturnHelper().gotoHomePage();  //создание контакта
+    if(!applicationManager.getContactHelper().thereAContact()){
+      applicationManager.getNavigationClass().gotoNewContact();
+      applicationManager.getContactHelper().createContact();
+      applicationManager.getReturnHelper().gotoHomePage();
     }
     applicationManager.getGroupHelper().selectGroup();
     applicationManager.getContactHelper().deleteContact();

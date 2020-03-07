e45b6c512b80c8a18752ed67976f666e98876d79
ZmixZ <termixator@yandex.ru>
2020-03-07 10:54:52 +0300

    Задание 9

--- addressbook-web-test/src/test/java/ru/solutionfirstprog/addressbook/tests/ContactModification.java
+++ addressbook-web-test/src/test/java/ru/solutionfirstprog/addressbook/tests/ContactModification.java
@@ -30,12 +30,15 @@ public class ContactModification extends TestBase {
         }
         List<ContactIng> before = applicationManager.getContactHelper().contactList();
         applicationManager.getContactHelper().tookContactModification();
-        ContactIng contact = new ContactIng("RZD", "Moscow, street Tambovskaya.", "jonjolli@yandex.fu", "Ivan", "Ivanovich", "Ivanov", null);
+        ContactIng contact = new ContactIng(before.get(before.size() - 1).getId(),"RZD", "Moscow, street Tambovskaya.", "jonjolli@yandex.fu", "Ivan", "Ivanovich", "Ivanov", null);
         applicationManager.getContactHelper().createContactInf(contact, false);
         applicationManager.getContactHelper().submitContactModification();
         applicationManager.getReturnHelper().gotoHomePage();
         List<ContactIng> after = applicationManager.getContactHelper().contactList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(contact);
 
         Comparator<ContactIng> maxId = (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
         after.sort(maxId);

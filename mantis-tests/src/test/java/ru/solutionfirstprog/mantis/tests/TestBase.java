81bd8147ad572875c1c44e7385e97c31b8e5f79d
ZmixZ <termixator@yandex.ru>
2020-03-29 20:50:02 +0300

    Задание 17

---
+++ mantis-tests/src/test/java/ru/solutionfirstprog/mantis/tests/TestBase.java
@@ -0,0 +1,22 @@
+package ru.solutionfirstprog.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.solutionfirstprog.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+    protected static final ApplicationManager applicationManager =
+            new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        applicationManager.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        applicationManager.stop();
+    }
+}

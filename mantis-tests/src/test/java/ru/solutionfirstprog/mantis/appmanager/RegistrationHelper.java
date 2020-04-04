1ff8c71629957f411a176cb35da52b02ea4c57a5
ZmixZ <termixator@yandex.ru>
2020-04-04 13:12:22 +0300

    Задание 18 промежуточное

--- mantis-tests/src/test/java/ru/solutionfirstprog/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/solutionfirstprog/mantis/appmanager/RegistrationHelper.java
@@ -20,6 +20,7 @@ public class RegistrationHelper extends HelperBase {
         wd.get(confirmationLink);
         type(By.name("password"), password);
         type(By.name("password_confirm"), password);
-        click(By.cssSelector("input[value='Изменить учетную запись']"));
+        click(By.cssSelector("span[class='bigger-110']"));
+        //By.cssSelector("input[value = '" + cont.getId() + "']")).
     }
 }

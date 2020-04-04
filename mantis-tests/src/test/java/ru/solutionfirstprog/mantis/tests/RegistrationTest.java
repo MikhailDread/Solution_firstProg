e3005fd8f3189ac1bb31a4cc97b7ac91fc6faea9
ZmixZ <termixator@yandex.ru>
2020-04-04 13:44:13 +0300

    Задание 18 промежуточное

--- mantis-tests/src/test/java/ru/solutionfirstprog/mantis/tests/RegistrationTest.java
+++ mantis-tests/src/test/java/ru/solutionfirstprog/mantis/tests/RegistrationTest.java
@@ -15,25 +15,25 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTest extends TestBase{
 
-    @BeforeMethod
+   // @BeforeMethod
     public void startMailServer(){
         applicationManager.mail().start();
     }
     @Test
     public void testRegistration() throws IOException, MessagingException {
-        //long now = System.currentTimeMillis();
-        //String user1 = String.format("user%s", now);
-        String user1 = "erta13";
+        long now = System.currentTimeMillis();
+        String user1 = String.format("mantisUser%s", now);
+        //String user1 = "erta13";
         String password = "erta13";
-        //String email = String.format("user%s@localhost.localdomain", now);
-        String email = "erta13@localhost.localdomain";
-        // applicationManager.james().createUser(user1, password);
+        String email = String.format("mantisUser%s@localhost", now);
+        //String email = "erta13@localhost.localdomain";
+        applicationManager.james().createUser(user1, password);
         applicationManager.registration().start(user1, email);
-        List<MailMessage> mailMessages = applicationManager.mail().waitForMail(2, 10000);
-        //List<MailMessage> mailMessages = applicationManager.james().waitForMail(user1, password,60000);
+        //List<MailMessage> mailMessages = applicationManager.mail().waitForMail(2, 10000);
+        List<MailMessage> mailMessages = applicationManager.james().waitForMail(user1, password,60000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         applicationManager.registration().finish(confirmationLink, password);
-        assertTrue(applicationManager.httpSession().login(user1));
+        //assertTrue(applicationManager.httpSession().login(user1));
     }
 
     private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
@@ -42,7 +42,7 @@ public class RegistrationTest extends TestBase{
         return regex.getText(mailMessage.text);
     }
 
-    @AfterMethod(alwaysRun = true)
+   // @AfterMethod(alwaysRun = true)
     public void stopMailServer(){
         applicationManager.mail().stop();
     }

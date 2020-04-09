78cb6afb6ba6d2af832c0aec9046f9362309c757
ZmixZ <termixator@yandex.ru>
2020-04-09 14:48:39 +0300

    Задание 20

--- rest-sample/src/test/java/ru/solutionfirstprog/rest/Issue.java
+++ rest-sample/src/test/java/ru/solutionfirstprog/rest/Issue.java
@@ -1,6 +1,7 @@
 package ru.solutionfirstprog.rest;
 
 
+import java.util.Objects;
 
 public class Issue {
 
@@ -33,5 +34,20 @@ public class Issue {
     public Issue withDescription(String description) {
         this.description = description;
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Issue issue = (Issue) o;
+        return id == issue.id &&
+                Objects.equals(subject, issue.subject) &&
+                Objects.equals(description, issue.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, subject, description);
     }
 }
